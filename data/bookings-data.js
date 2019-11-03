55bdb6d3040e8f6b1a79b195510ad38779eb23f7
Dustin Mikusko <dustinjm713@yahoo.com>
2019-11-03 16:26:58 -0700

    Add mock data files for testing

---
+++ data/bookings-data.js
@@ -0,0 +1,235 @@
+const bookings = [
+  {
+  id: 1572293130156,
+  userID: 19,
+  date: "2019/11/06",
+  roomNumber: 18,
+  roomServiceCharges: [ ]
+  },
+  {
+  id: 1572293130159,
+  userID: 21,
+  date: "2019/11/12",
+  roomNumber: 8,
+  roomServiceCharges: [ ]
+  },
+  {
+  id: 1572293130159,
+  userID: 12,
+  date: "2019/10/29",
+  roomNumber: 10,
+  roomServiceCharges: [ ]
+  },
+  {
+  id: 1572293130159,
+  userID: 27,
+  date: "2019/11/15",
+  roomNumber: 4,
+  roomServiceCharges: [ ]
+  },
+  {
+  id: 1572293130160,
+  userID: 16,
+  date: "2019/11/06",
+  roomNumber: 7,
+  roomServiceCharges: [ ]
+  },
+  {
+  id: 1572293130160,
+  userID: 8,
+  date: "2019/11/22",
+  roomNumber: 1,
+  roomServiceCharges: [ ]
+  },
+  {
+  id: 1572293130160,
+  userID: 3,
+  date: "2019/11/22",
+  roomNumber: 9,
+  roomServiceCharges: [ ]
+  },
+  {
+  id: 1572293130160,
+  userID: 47,
+  date: "2019/12/09",
+  roomNumber: 14,
+  roomServiceCharges: [ ]
+  },
+  {
+  id: 1572293130160,
+  userID: 20,
+  date: "2019/12/01",
+  roomNumber: 23,
+  roomServiceCharges: [ ]
+  },
+  {
+  id: 1572293130160,
+  userID: 11,
+  date: "2019/11/22",
+  roomNumber: 8,
+  roomServiceCharges: [ ]
+  },
+  {
+  id: 1572293130160,
+  userID: 23,
+  date: "2019/12/07",
+  roomNumber: 16,
+  roomServiceCharges: [ ]
+  },
+  {
+  id: 1572293130160,
+  userID: 44,
+  date: "2019/11/16",
+  roomNumber: 12,
+  roomServiceCharges: [ ]
+  },
+  {
+  id: 1572293130160,
+  userID: 7,
+  date: "2019/11/01",
+  roomNumber: 12,
+  roomServiceCharges: [ ]
+  },
+  {
+  id: 1572293130160,
+  userID: 39,
+  date: "2019/11/13",
+  roomNumber: 21,
+  roomServiceCharges: [ ]
+  },
+  {
+  id: 1572293130160,
+  userID: 46,
+  date: "2019/12/03",
+  roomNumber: 2,
+  roomServiceCharges: [ ]
+  },
+  {
+  id: 1572293130160,
+  userID: 5,
+  date: "2019/12/14",
+  roomNumber: 11,
+  roomServiceCharges: [ ]
+  },
+  {
+  id: 1572293130160,
+  userID: 45,
+  date: "2019/11/10",
+  roomNumber: 16,
+  roomServiceCharges: [ ]
+  },
+  {
+  id: 1572293130160,
+  userID: 25,
+  date: "2019/12/01",
+  roomNumber: 9,
+  roomServiceCharges: [ ]
+  },
+  {
+  id: 1572293130160,
+  userID: 30,
+  date: "2019/11/06",
+  roomNumber: 11,
+  roomServiceCharges: [ ]
+  },
+  {
+  id: 1572293130160,
+  userID: 49,
+  date: "2019/11/16",
+  roomNumber: 5,
+  roomServiceCharges: [ ]
+  },
+  {
+  id: 1572293130160,
+  userID: 18,
+  date: "2019/12/16",
+  roomNumber: 16,
+  roomServiceCharges: [ ]
+  },
+  {
+  id: 1572293130160,
+  userID: 34,
+  date: "2019/11/10",
+  roomNumber: 12,
+  roomServiceCharges: [ ]
+  },
+  {
+  id: 1572293130160,
+  userID: 31,
+  date: "2019/11/17",
+  roomNumber: 1,
+  roomServiceCharges: [ ]
+  },
+  {
+  id: 1572293130160,
+  userID: 26,
+  date: "2019/12/15",
+  roomNumber: 22,
+  roomServiceCharges: [ ]
+  },
+  {
+  id: 1572293130160,
+  userID: 10,
+  date: "2019/11/24",
+  roomNumber: 12,
+  roomServiceCharges: [ ]
+  },
+  {
+  id: 1572293130160,
+  userID: 1,
+  date: "2019/11/18",
+  roomNumber: 5,
+  roomServiceCharges: [ ]
+  },
+  {
+  id: 1572293130161,
+  userID: 9,
+  date: "2019/11/07",
+  roomNumber: 5,
+  roomServiceCharges: [ ]
+  },
+  {
+  id: 1572293130161,
+  userID: 22,
+  date: "2019/11/15",
+  roomNumber: 15,
+  roomServiceCharges: [ ]
+  },
+  {
+  id: 1572293130161,
+  userID: 35,
+  date: "2019/12/06",
+  roomNumber: 24,
+  roomServiceCharges: [ ]
+  },
+  {
+  id: 1572293130161,
+  userID: 14,
+  date: "2019/11/28",
+  roomNumber: 23,
+  roomServiceCharges: [ ]
+  },
+  {
+  id: 1572293130161,
+  userID: 28,
+  date: "2019/12/06",
+  roomNumber: 14,
+  roomServiceCharges: [ ]
+  },
+  {
+  id: 1572293130161,
+  userID: 15,
+  date: "2019/12/06",
+  roomNumber: 5,
+  roomServiceCharges: [ ]
+  },
+  {
+  id: 1572293130161,
+  userID: 48,
+  date: "2019/11/14",
+  roomNumber: 18,
+  roomServiceCharges: [ ]
+  }
+]
+
+export default bookings;
