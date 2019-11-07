58786cacb0dbea0c0e8997ee9ce3104a1040bf35
Dustin Mikusko <dustinjm713@yahoo.com>
2019-11-06 17:01:48 -0700

    Add spy test for Manager class

--- test/manager-test.js
+++ test/manager-test.js
@@ -10,10 +10,20 @@ import users from '../data/users-data';
 describe('Manager', () => {
   let hotel;
   let manager;
+  let fetchSpy;
 
   beforeEach(() => {
-    hotel = new Hotel(bookings, rooms)
+    hotel = new Hotel(bookings, rooms);
     manager = new Manager(bookings, rooms, users);
+    fetchSpy = chai.spy.on(global, 'fetch', () => {
+      return new Promise((resolve, reject) => {
+        resolve({message: 'Data has been fetched.'});
+      })
+    });
+  });
+
+  afterEach(() => {
+    chai.spy.restore(fetchSpy);
   });
 
   it('should be able to find a specific user', () => {
@@ -24,13 +34,18 @@ describe('Manager', () => {
   });
 
   it('should be able to find a specific booking id by date and room number', () => {
-    expect(manager.findBooking(48,'2019/11/14', 18)).to.equal(1572293130161)
+    chai.spy.on(manager, "returnUpcomingBookings", () => [{
+      id: 1572293130161,
+      userID: 48,
+      date: '2019/11/14',
+      roomNumber: 18,
+      roomServiceCharges: [] 
+    }]);
+    expect(manager.findBooking(48,'2019/11/14', 18)).to.equal(1572293130161);
   })
 
   it('should be able to delete an upcoming booking for user', () => {
-    
-  })
-  
-
-
+    manager.deleteBooking(1573080561901);
+    expect(fetchSpy).to.have.been.called(1);
+  });
 })
